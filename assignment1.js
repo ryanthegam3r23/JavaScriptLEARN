2315967dbcfcb322f94d37b2a8512d7c146eea21
Your Name <you@example.com>
2025-05-09 15:43:59 -0500

    First Assignment

---
+++ assignment1.js
@@ -0,0 +1,32 @@
+console.log("Assignment 1");
+
+//1. Declare Variables
+//Using let
+let character = "Bob";
+let action = "kick it";
+let object = "banana";
+let place = "the moon";
+let time = "a summer afternoon";
+
+// Using "const"
+const occasion = "his immagination";
+
+//2. Create the story
+console.log("--- Funny Story ---");
+console.log(character + " was on " + place + " on " + time + ".");
+console.log("Suddenly, " + character + " saw " + object + " and decided to " + action + ".");
+console.log("It turend out to be " + occasion + " and he was really upset" + ".");
+
+//3. Experiment with Different Values
+console.log("--- Different Version of the story ---")
+
+// Change values to create a different story
+character = "Mark";
+action = "ride it";
+object = "unicorn";
+place = "the sun";
+time = "a summer evening" ;
+
+console.log(character + " was on " + place + " on " + time + ".");
+console.log("Suddenly, " + character + " saw " + object + " and decided to " + action + ".");
+console.log("It turned out to be " + occasion + " and he stared to cry" + ".");
---
+++ variables.js
@@ -0,0 +1,72 @@
+console.log("Hello World CH#58");
+
+// This is a comment in JS
+console.log("Hello my name is Ryan and i am 20 years old");
+
+// STRING VARIABLES
+// keywork variablename = value;
+let name = "Peter";
+let lastName = "Parker";
+
+console.log(name);
+console.log(lastName);
+console.log("Hello my name is " + name +  " " + lastName  );
+
+// CREATING NUMERICAL VARIABLES
+let age = 40;
+let grade = 9.3;
+
+console.log(age);
+console.log("I am " + age + " years old");
+console.log(grade);
+
+
+
+// BOOLEAN VARIABLES(true/false)
+let isAProfessor = true;
+let isAStudent = false;
+let isAdmin = true;
+
+console.log(isAProfessor);
+console.log(isAStudent);
+
+
+
+
+
+// MATH OPERATIONS
+let number1 = 8.3;
+let number2 = 4;
+let additon = number1 + number2;
+
+console.log(additon);
+console.log(number1 + " + " + number2 + " = " + (number1 + number2));
+
+// Substraction
+let substraction = (number1 - number2);
+console.log(number1 + " - " + number2 + " = " + substraction.toFixed(2))
+
+
+// Multiplication
+let multiplication = (number1 * number2);
+console.log(number1 + " * " + number2 + " = " + multiplication);
+
+// Change variable value
+
+
+
+
+
+
+
+// LET VS VAR
+var school = "SDKGU";
+var school = "ITSCHOOL"
+console.log(school);
+
+// CONSRANT VARIBLES
+const PI = 3.1416;
+const myName = "Ryan";
+
+console.log(PI);
+console.log(myName);
